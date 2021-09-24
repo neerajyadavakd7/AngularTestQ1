4e07398fb7d3edd8ce00b0391b6d92120496fd9a
Neeraj <nyadav1@q3tech.com>
2021-09-24 14:08:49 +0530

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,80 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MatTableDataSource } from '@angular/material/table';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'AngularTest';
+
+
+export class AppComponent implements OnInit{
+  
+  displayedColumns: string[] = ['ToDoList', 'DeleteToDoItem'];
+   todolist : ToDoList[] = [];
+   item : ToDoList = {id : 0, value : ''};
+   dataSource = new MatTableDataSource();
+   id : number = 1;
+   editId : number = 0;
+
+   ngOnInit(): void {
+    this.SetToDoList();
+    this.dataSource.data = this.ShowAllList();
+  }
+
+  SetToDoList() 
+  { 
+    if(!localStorage.getItem("ToDoList"))
+    localStorage.setItem('ToDoList', '{}');
+  }
+   AddToDoItem( value : string, code : number,  event? : KeyboardEvent)
+   {
+      this.item.id = this.id;
+      this.item.value = value;
+     if(event?.key === 'Enter' && code == 1 && value != '')
+     {
+      this.todolist.push(this.item);
+      this.id ++;
+     }
+     else if(code == 2 && value != '')
+     {
+      this.todolist.push(this.item);
+      this.id ++;
+     }
+     localStorage.setItem('ToDoList', JSON.stringify(this.todolist));
+     this.dataSource.data = this.ShowAllList();
+     
+   }
+
+   DeleteToDoItem(item: ToDoList)
+   {
+     var index = this.todolist.findIndex(x => x.id === item.id);
+     this.todolist.splice(index,1);
+    localStorage.setItem('ToDoList', JSON.stringify(this.todolist));
+    this.dataSource.data = this.ShowAllList();
+   }
+
+   EditToDoItem(item : ToDoList, value : string)
+   {
+    this.todolist[this.todolist.findIndex(x => x.id === item.id)].value = value;
+    localStorage.setItem('ToDoList', JSON.stringify(this.todolist));
+    this.editId = 0;
+    this.dataSource.data = this.ShowAllList();
+   }
+
+    ShowAllList() : String[]
+   {
+      var storedToDoList = JSON.parse(localStorage.getItem("ToDoList") || '{}');
+      if(Object.keys(storedToDoList).length === 0)
+      this.todolist = [];
+      else
+      this.todolist = storedToDoList;
+      return storedToDoList;
+   }
+}
+
+export interface ToDoList
+{
+  id : number;
+  value : string;
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
+import { MatTableModule } from '@angular/material/table'
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatIconModule} from '@angular/material/icon';
+import { CommonModule } from '@angular/common';
 
 @NgModule({
   declarations: [
@@ -10,7 +12,10 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    MatTableModule,
+    MatIconModule,
+    CommonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
